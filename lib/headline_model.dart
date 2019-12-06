5c60b110b0fd7a06d231ae6b8c8a14c3541dc5f9
Geeky Shows <samarpitabhaumik2017@gmail>
2019-12-06 06:48:29 -0800

    this is samarpita

---
+++ lib/headline_model.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+
+
+class HeadlineModel extends StatelessWidget {
+
+
+  final String headlineNews;
+
+  const HeadlineModel({Key key,
+
+    @required this.headlineNews,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+           Padding(
+            padding: const EdgeInsets.all(12.0),
+
+            child: Text(headlineNews,style: TextStyle(
+              fontSize: 15.0,
+            ),
+            ),
+
+        );
+
+  }
+}
